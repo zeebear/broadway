b3f8045d19951ee1b620dc9d747dd40203d27905
Claire Fahy <zeebear21@gmail.com>
2019-04-27 16:38:36 -0300

    create Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_27_183354) do
+ActiveRecord::Schema.define(version: 2019_04_27_193812) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
