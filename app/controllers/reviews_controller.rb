aae1946c6a0e6e9d644fa50e32a51bd33a009a45
Claire Fahy <zeebear21@gmail.com>
2019-04-29 20:53:45 -0300

    add view files for reviews and render on play show page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :set_play, only: %i[new create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.play = @play
+    @review.user = current_user
+
+    rediretc_to play_path(@play) if @review.save
+    return
+
+    render 'new'
+  end
+
+  private
+
+  def set_play
+    @play = Play.find(params[:play_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
 end
